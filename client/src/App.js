03f4ebadc1b81557287fa01191b5684ae69ef741
aman-saraiya <amansaraiya937@gmail.com>
2022-11-20 03:19:42 +0530

    Created a basic register page

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
-
+import { ToastContainer } from "react-toastify";
 const App = () => {
   return (
     <>
+      <ToastContainer></ToastContainer>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Button } from 'antd'
+import { toast } from 'react-toastify'
 
 const Register = () => {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    profilePicture: "",
+  })
+
+  const validateUserDetails = () => {
+
+  }
+  const handleInputChange = (event) => {
+    setUser((prevState) => ({
+      prevState,
+      [event.target.name]: event.target.value,
+    }))
+    console.log(event);
+  } 
+  const handleRegistration = (event)=>{
+    toast.success("User Registered Successfully", {})    
+    console.log(event);
+  }
   return (
-    <div>Register</div>
+    <div>
+      <form>
+        <div className='form-group'>
+          <input type="text" className='form-control' name="firstName" onChange={handleInputChange} placeholder="First Name" />
+        </div>
+        <div className='form-group'>
+          <input type="text" className='form-control' name="lastName" onChange={handleInputChange} placeholder="Last Name" />
+        </div>
+        <div className='form-group'>
+          <input type="email" className='form-control' name="email" onChange={handleInputChange} placeholder="Email" />
+        </div>
+        <div className='form-group'>
+          <input type="file" className='form-control' name="profilePicture" onChange={handleInputChange}/>
+        </div>
+        <Button onClick={handleRegistration}>Register</Button>
+      </form>
+    </div>
   )
 }
 
