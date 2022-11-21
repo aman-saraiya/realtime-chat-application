ecf12fad319aa0518b6807767391198d2c11739c
aman-saraiya <amansaraiya937@gmail.com>
2022-11-21 15:05:27 +0530

    Login with email-pass and google added

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { Button } from "antd";
-import { EyeOutlined, EyeInvisibleOutlined, GoogleOutlined } from "@ant-design/icons";
+import {
+  EyeOutlined,
+  EyeInvisibleOutlined,
+  GoogleOutlined,
+} from "@ant-design/icons";
+import { toast } from "react-toastify";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { auth, googleAuthProvider } from "../../firebase";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -8,6 +15,30 @@ const Login = () => {
 
   const handlePasswordVisiblility = () => {
     setPasswordVisible((prevState) => !prevState);
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      const { user } = result;
+      const idTokenResult = await user.getIdTokenResult();
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleAuthProvider);
+      const { user } = result;
+      console.log(user);
+      const idTokenResult = await user.getIdTokenResult();
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
   };
   return (
     <div>
@@ -32,18 +63,27 @@ const Login = () => {
               onChange={(event) => setPassword(event.target.value)}
               placeholder="Password"
             />
-            <Button className="input-group-text" onClick={handlePasswordVisiblility} disabled={!password}>
+            <Button
+              className="input-group-text"
+              onClick={handlePasswordVisiblility}
+              disabled={!password}
+            >
               {passwordVisible ? <EyeInvisibleOutlined /> : <EyeOutlined />}
             </Button>
           </div>
         </div>
-        <Button>Login</Button>
+        <Button onClick={handleLogin} disabled={!email || password.length < 6}>
+          Login
+        </Button>
+        <a href="/forgot-password">Forgot Password?</a>
         <div>
           Don't have an account? <a href="/register">Sign Up</a>
         </div>
-        <hr data-content="OR" class="hr-text"/>
+        <hr data-content="OR" className="hr-text" />
         <div className="form-group">
-          <Button className=""><GoogleOutlined /></Button>
+          <Button onClick={handleGoogleLogin} className="">
+            <GoogleOutlined />
+          </Button>
         </div>
       </form>
     </div>
