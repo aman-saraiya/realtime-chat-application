a0a5dcf34c5fb360f17ea4cb1f3825a74e73712c
aman-saraiya <amansaraiya937@gmail.com>
2022-11-23 14:16:21 +0530

    added chat routes backend

---
+++ server/routes/chatRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+// import middleware
+const { authCheck } = require("../middleware/auth");
+
+// import controllers
+const {
+  fetchChats,
+  createOrFetchPersonalChat,
+  createGroupChat,
+  addUserToGroup,
+  removeUserFromGroup,
+  renameGroup,
+} = require("../controllers/chatControllers");
+
+router.get("/chat/:userId", authCheck, fetchChats);
+router.post("/chat/:userId", authCheck, createOrFetchPersonalChat);
+router.post("/chat/group/create", authCheck, createGroupChat);
+router.put("/chat/group/addUser/", authCheck, addUserToGroup);
+router.put("/chat/group/removeUser/", authCheck, removeUserFromGroup);
+router.put("/chat/group/renameGroup/", authCheck, renameGroup);
+
+module.exports = router;
