33b27ace807af8ddb2c136aea9686c7dab243321
aman-saraiya <amansaraiya937@gmail.com>
2022-11-26 04:10:58 +0530

    Added the send button

--- client/src/components/forms/SendMessage.jsx
+++ client/src/components/forms/SendMessage.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import { SendOutlined } from "@ant-design/icons";
+import React, { useState } from "react";
 
 const SendMessage = () => {
+  const [messageContent, setMessageContent] = useState("");
+  const handleInputChange = (event) => {
+    setMessageContent(event.target.value);
+  };
+
+  const handleSendMessage = () => {};
   return (
     <div className="row p-0 m-0" style={{ height: "8%" }}>
-      <input style={{ width: "100%", border: "1px solid red" }} type="text" />
+      {/* <input style={{ width: "100%", border: "1px solid red" }} type="text" /> */}
+      <div className="input-group p-0 m-0">
+        <input
+          style={{ width: "100%", border: "none" }}
+          type="text"
+          value={messageContent}
+          onChange={handleInputChange}
+        />
+        <SendOutlined
+          style={{ background: "white", fontSize: "25px", cursor: "pointer" }}
+        />
+      </div>
     </div>
   );
 };
