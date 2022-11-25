ea038abdc49b48013da8c26a5bb947e44b5d06ed
aman-saraiya <amansaraiya937@gmail.com>
2022-11-26 01:12:20 +0530

    Added chat header elements

--- client/src/components/navbar/ChatsHeader.jsx
+++ client/src/components/navbar/ChatsHeader.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Menu } from "antd";
-import { SettingOutlined, BellFilled, LogoutOutlined } from "@ant-design/icons";
+import { Menu, Avatar, Image, Dropdown, Button } from "antd";
+import {
+  SettingOutlined,
+  BellFilled,
+  LogoutOutlined,
+  GroupOutlined,
+  UsergroupAddOutlined,
+} from "@ant-design/icons";
 import { auth } from "../../firebase";
 import { signOut } from "firebase/auth";
 const ChatsHeader = () => {
@@ -11,11 +17,40 @@ const ChatsHeader = () => {
     window.localStorage.removeItem("user");
     navigate("/login");
   };
+  const user = JSON.parse(window.localStorage.getItem("user"));
+  const items = [
+    { label: "View Profile", key: "view-profile" }, // remember to pass the key prop
+    { label: "Sign Out", key: "sign-out" },
+  ];
   return (
     <div className="row p-0 m-0" style={{ height: "8%" }}>
-      <h2 className="p-0 m-0" style={{ border: "1px solid red" }}>
-        Chat Header
-      </h2>
+      <div className="col-1 p-0 m-0" style={{ border: "3px solid green" }}>
+        <Avatar
+          src={user.profilePicture}
+          size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 40 }}
+        />
+      </div>
+      <div className="col">{user.name}</div>
+      <div
+        className="col-1 p-0 m-0 float-end"
+        style={{ border: "1px solid red" }}
+      >
+        <Button shape="circle">
+          <UsergroupAddOutlined />
+        </Button>
+      </div>
+      <div
+        className="col-1 p-0 m-0 float-end"
+        style={{ border: "1px solid red" }}
+      >
+        <Dropdown placement="bottomRight" menu={{ items }}>
+          <a>
+            <Button shape="circle">
+              <i class="fa-solid fa-ellipsis-vertical"></i>
+            </Button>
+          </a>
+        </Dropdown>
+      </div>
     </div>
   );
 };
