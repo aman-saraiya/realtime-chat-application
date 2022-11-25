24473e0990f2a519337b70bab036a201a6ce851e
aman-saraiya <amansaraiya937@gmail.com>
2022-11-25 17:10:33 +0530

    Added back button for smaller screens

--- client/src/components/navbar/MessageHeader.jsx
+++ client/src/components/navbar/MessageHeader.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { ArrowLeftOutlined } from "@ant-design/icons";
+import { ChatsState } from "../context/ChatsProvider";
 const MessageHeader = () => {
+  const { isScreenSmall, selectedChat, setSelectedChat } = ChatsState();
+  const handleBackClick = () => {
+    setSelectedChat();
+  };
   return (
     <div
       className="row m-0 p-0"
       style={{ height: "8%", border: "1px solid red" }}
     >
-      <button className="border-0 m-0 p-0" style={{ width: "4%" }}>
+      <button
+        className="border-0 m-0 p-0"
+        style={{ width: "4%", display: isScreenSmall ? "" : "none" }}
+        onClick={handleBackClick}
+      >
         <ArrowLeftOutlined style={{ fontSize: "20px" }} />
       </button>
       <h2 className="col m-0 p-0">Message Header</h2>
