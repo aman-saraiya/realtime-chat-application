b0772cbed9783eb6919de07f61fbf91459b6c324
aman-saraiya <amansaraiya937@gmail.com>
2022-12-18 02:31:27 +0530

    Styled the register page

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import { auth } from "../../firebase";
 import { sendSignInLinkToEmail } from "firebase/auth";
 import { toast } from "react-toastify";
+import AuthHOC from "./AuthHOC";
+import AppPreviewSection from "./AppPreviewSection";
+import FormSection from "./FormSection";
 
 const Register = () => {
   const [email, setEmail] = useState("");
@@ -26,20 +29,41 @@ const Register = () => {
     setEmail("");
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <input
-            type="email"
-            className="form-control"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            placeholder="Email"
-          />
-          <Button onClick={handleSubmit}>Register</Button>
-        </div>
-      </form>
-    </div>
+    <AuthHOC>
+      <AppPreviewSection />
+      <FormSection>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="d-flex">
+            <input
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              className="auth-form-input flex-grow-1"
+              placeholder="Email"
+            />
+          </div>
+          <div className="d-flex justify-content-end">
+            <button
+              style={{
+                display: "flex",
+                alignItems: "center",
+                padding: "0.5rem",
+                backgroundColor: "#19bd06",
+                color: "#ffffff",
+                outline: "none",
+                border: "none",
+                height: "1.4rem",
+                fontSize: "0.8rem",
+              }}
+              disabled={!email}
+              onClick={handleSubmit}
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </FormSection>
+    </AuthHOC>
   );
 };
 
