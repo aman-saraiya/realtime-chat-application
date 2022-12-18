7c69bc5412ac5ee8dc9b49316cc6fab28fe194f5
aman-saraiya <amansaraiya937@gmail.com>
2022-12-18 17:36:03 +0530

    Forgot password component styled

--- client/src/pages/auth/ForgotPassword.jsx
+++ client/src/pages/auth/ForgotPassword.jsx
@@ -1,8 +1,10 @@
-import { Button } from "antd";
 import { sendPasswordResetEmail } from "firebase/auth";
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import { auth } from "../../firebase";
+import AppPreviewSection from "../auth/AppPreviewSection";
+import AuthHOC from "../auth/AuthHOC";
+import FormSection from "../auth/FormSection";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
@@ -17,23 +19,42 @@ const ForgotPassword = () => {
     toast.success(`Password reset link sent to ${email}`);
   };
   return (
-    <div>
-      <form onSubmit={handlePasswordReset}>
-        <div className="form-group">
-          <input
-            type="email"
-            name="email"
-            value={email}
-            className="form-control"
-            onChange={(event) => setEmail(event.target.value)}
-            placeholder="Email"
-          />
-        </div>
-        <Button onClick={handlePasswordReset} disabled={!email}>
-          Send Password Reset Link
-        </Button>
-      </form>
-    </div>
+    <AuthHOC>
+      <AppPreviewSection />
+      <FormSection>
+        <form className="login-form" onSubmit={handlePasswordReset}>
+          <div className="d-flex">
+            <input
+              type="email"
+              name="email"
+              value={email}
+              className="auth-form-input flex-grow-1"
+              onChange={(event) => setEmail(event.target.value)}
+              placeholder="Email"
+            />
+          </div>
+          <div className="d-flex" style={{ justifyContent: "flex-end" }}>
+            <button
+              style={{
+                display: "flex",
+                alignItems: "center",
+                padding: "0.5rem",
+                backgroundColor: "#19bd06",
+                color: "#ffffff",
+                outline: "none",
+                border: "none",
+                height: "1.4rem",
+                fontSize: "0.8rem",
+              }}
+              onClick={handlePasswordReset}
+              disabled={!email}
+            >
+              Send Password Reset Link
+            </button>
+          </div>
+        </form>
+      </FormSection>
+    </AuthHOC>
   );
 };
 
